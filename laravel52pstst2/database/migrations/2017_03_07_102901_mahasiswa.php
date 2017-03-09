8a992a8554d2d775fbd2c3ee090c210644b575b8
wildan <wildantamvan@gmail.com>
2017-03-09 13:06:07 +0800

    postest 2

---
+++ laravel52pstst2/database/migrations/2017_03_07_102901_mahasiswa.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Mahasiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('mahasiswa', function(Blueprint $table)
+    {
+        $table->integer('id',10,true,true);
+        $table->string('nama',50);
+        $table->string('nim',10);
+        $table->text('alamat');
+        $table->integer('pengguna_id',false,true);
+        $table->foreign('pengguna_id')->references('id')->on('pengguna');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
